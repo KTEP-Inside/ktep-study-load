c7812628962e975fd13398bd59cd2460c8bdcdca
mamba <jc.bob1412@gmail.com>
2023-12-19 13:26:42 +0300

    first template table, create utils

--- study_load/table_creator/views.py
+++ study_load/table_creator/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .services import add_data
+from .models import *
+from .utils import *
+
 
 def index(request):
+    type_load = TypeLoad.objects.all()
+    n = len(type_load)
     context = {
         'title': 'Creator',
+        'type_load': type_load,
+        "type_load_length": n,
+        'type_results': type_results
     }
     add_data()
     return render(request, template_name='table_creator/table_creator.html', context=context)
